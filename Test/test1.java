f896e79b47057ac1b72d84c31ce21a007dc395e2
Barry Wang <j795wang@uwaterloo.ca>
2021-02-07 14:01:05 -0500

    add

---
+++ Test/test1.java
@@ -0,0 +1,12 @@
+class test1 {
+    static int count = 0;
+    public static void main(String[] args) {
+        if(count < 3){
+            count++;
+            main(null);
+        }else{
+            return;
+        }
+        System.out.println("Hello World!");
+    }
+}
